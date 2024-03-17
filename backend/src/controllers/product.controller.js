1ddd3a1f02ab2440b41ce8fa2ece3344e92ce7cf
Nguyen Thi Thu Trang <trang42a1dqh@gmail.com>
2024-03-18 02:04:41 +0700

    test commit

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,55 +1,66 @@
-var product = require('../models/product.model')
-exports.get_list = function(req, res){
-    db.query("SELECT * FROM products", function(err, product)
-    { if(err) {
-        return res.json(err)
-    } else {
-        return res.send(product)
+var {database} = require("../common/connect")
+const { QueryTypes } = require("sequelize")
+
+exports.get_list = async function(req, res){
+    try {
+        const productList = await database.query("SELECT * FROM products", {type: QueryTypes.SELECT})
+    return res.status(200).json(productList);
+    } catch (error) {
+        return res.status(400).json({msg: error})
     }
-    })
-}
-exports.detail = function(req, res){
-    const product_id = rq.params.id
-    db.query("SELECT * FROM products WHERE product_id = ?",product_id, function(err, product) {
-        if(err || product.length == 0) {
-            return res.json("Không tồn tại sản phẩm")
-        } else {
-            return res.send(product)
-        }
-       })
 }
 
-exports.add_product = function(req,res) {
-    const category_id = req.body.category_id
-    const product_name = req.body.product_name
-    const price = req.body.price
-    const quantity = req.body.quantity
-    const thumbnail = req.body.thumbnail
-    const description = req.body.description
-    const status = req.body.status
-    db.query("INSERT INTO products (category_id, product_name, price, quantity, thumbnail, description, status) VALUES (?,?,?,?,?,?,?)", [category_id, product_name, price, quantity, thumbnail, description, status], (err, result) => {
-            if(err) {console.log(err)}
-            else res.json("Bạn đã thêm sản phẩm thành công")
-    })
+exports.detail = async function(req, res){
+    try {
+        const product_id = req.params.id
+        const productDetail = await database.query("SELECT * FROM products WHERE product_id = :product_id", {
+            replacements: {
+                product_id: product_id
+            }, type: QueryTypes.SELECT
+        })
+        return res.status(200).json(productDetail);
+    } catch (error) {
+        return res.status(400).json({msg: error})
+    }
 }
 
-exports.remove_product = function(req,res) {
-    var product_id = req.params.id
-    db.query("DELETE FROM products WHERE product_id=?", product_id, function(err, products) {
-        if(err) {
-           return res.json(null)
-        } else {
-            return res.json("Xóa sản phẩm thành công")
-        }
-    }) 
+exports.add_product = async function(req,res) {
+    try {
+        await database.query("INSERT INTO products (category_id, product_name, price, quantity, thumbnail, description, status) VALUES (:category_id,:product_name,:price,:quantity,:thumbnail,:description,:status)", {
+            replacements: {
+                category_id: req.body.category_id, 
+                product_name: req.body.product_name, 
+                price: req.body.price, 
+                quantity: req.body.quantity, 
+                thumbnail: req.body.thumbnail, 
+                description: req.body.description, 
+                status: req.body.status
+            }, type: QueryTypes.INSERT
+        })
+        return res.status(200).json({msg: "Đã thêm sản phẩm thành công"});
+    } catch (error) {
+        return res.status(400).json({msg: error})
+    }
 }
 
-exports.update_product = function(req,res) {
-    db.query("UPDATE products SET category_id=?, product_name=?, price=?, quantity=?, thumbnail=?, description=?, status=? WHERE product_id = ?", [req.boy.category_id, req.body.product_name, req.body.price,req.body.quantity, req.body.thumbnail, req.body.description, data.status, req.body.product_id], function(err, product) {
-        if(err) {
-           return res.json(err, null)
-        } else {
-            return res.send("Cập nhật sản phẩm thành công")
-        }
-    }) 
+exports.update_product = async function(req,res) {
+    try {
+        await database.query("UPDATE products SET category_id=:category_id, product_name=:product_name, price=:price, quantity=:quantity, thumbnail=:thumbnail, description=:description, status=:status WHERE product_id = :product_id", 
+        {
+            replacements: {
+                category_id: req.body.category_id, 
+                product_name: req.body.product_name, 
+                price: req.body.price,
+                quantity: req.body.quantity, 
+                thumbnail: req.body.thumbnail, 
+                description: req.body.description, 
+                status: req.body.status, 
+                product_id: req.body.product_id
+            }, type: QueryTypes.UPDATE
+        })
+        return res.status(200).json({msg: "Đã cập nhật sản phẩm thành công"}); 
+    } catch (error) {
+        return res.status(400).json({msg: error})
+    }
+    
 }
