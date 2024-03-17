baeca80ec5dac21fde592021cfd156a35d1ceaeb
Nguyen Thi Thu Trang <trang42a1dqh@gmail.com>
2024-03-18 02:05:11 +0700

    test commit

--- backend/src/models/score.model.js
+++ backend/src/models/score.model.js
@@ -1,20 +1,61 @@
-const db = require('../common/connect')
-const physicalscore = function(physicalscore) {
-    this.mssv = physicalscore.mssv
-    this.fullname = physicalscore.fullname
-    this.class = physicalscore.class
-    this.univercity = physicalscore.univercity
-    this.football_score = physicalscore.football_score
-    this.bedminton_score = physicalscore.bedminton_score
-    this.tabletennis_score = physicalscore.tabletennis_score
-    this.basketball_score = physicalscore.basketball_score
-    this.air_volleyball_score = physicalscore.air_volleyball_score
-    this.volleyball_score = physicalscore.volleyball_score
-    this.taekwondo_score = physicalscore.taekwondo_score
-    this.golf_score = physicalscore.golf_score
-    this.CDR = physicalscore.CDR
-}
-
-
-module.exports = physicalscore
-
+const { database } = require("../common/connect");
+const { DataTypes } = require("sequelize");
+const Score = database.define(
+    "physicalscore",
+    {
+        mssv: {
+            type: DataTypes.INTEGER, 
+            allowNull: false, 
+            primaryKey: true
+        },
+        fullname: {
+            type: DataTypes.STRING, 
+            allowNull: true,
+        },
+        class: {
+            type: DataTypes.STRING, 
+            allowNull: true,
+        },
+        univercity: {
+            type: DataTypes.STRING, 
+            allowNull: true,
+        },
+        football_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        bedminton_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        tabletennis_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        basketball_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        air_volleyball_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        volleyball_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        taekwondo_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        golf_score: {
+            type: DataTypes.FLOAT, 
+            allowNull: true,
+        },
+        CDR : {
+            type: DataTypes.INTEGER, 
+            allowNull: true,
+        }
+    }
+)
+module.exports = Score
