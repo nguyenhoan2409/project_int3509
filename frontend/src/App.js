8d8f221f90ad4ebaa038f8bed1713f697a3b8b6f
Nguyen Thi Thu Trang <trang42a1dqh@gmail.com>
2024-03-19 16:46:41 +0700

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Home } from "~/Pages/Home/home";
 import { Request } from "./Pages/Request/Request";
-import { Uniform } from "./Pages/Uniform/Uniform";
+import { Score } from "./Pages/Score/Score";
 import { Profile } from "./Pages/Profile/Profile";
 import { ShopCategory } from "./Pages/Product/ShopCategory";
 import { Product } from "./Pages/Product/Product";
@@ -20,9 +20,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />}></Route>
-          <Route path='/uniform' element={<ShopCategory banner={uniform_banner} category="uniform" />}></Route>
-          <Route path='/request' element={<Request />}></Route>
-          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/score' element={<Score/>}></Route>
+          <Route path='/request' element={<Request/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
           <Route path='/product' element={<ShopCategory banner={uniform_banner} category="uniform" />}>  <Route path=':productId' element={<Product />}></Route>
           </Route>
 
