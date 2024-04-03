6417ceb34af029ef28b3ba7b415a89a55343a5de
Nguyen Thi Thu Trang <trang42a1dqh@gmail.com>
2024-04-04 00:13:31 +0700

    update

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Item.css'
+import { Link, useNavigate } from "react-router-dom";
 
 export const Item = ({ image, name, order }) => {
+
   return (
     <div className="product">
       <img src={image} alt={name} />
       <h2>{name}</h2>
       <button>{order}</button>
     </div>
+
   );
 };
 
