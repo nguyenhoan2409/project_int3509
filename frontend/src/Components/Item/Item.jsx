9d19785cad57e6a79a67b07a37b53ae0ffab4534
Nguyen Thi Thu Trang <trang42a1dqh@gmail.com>
2024-04-05 08:41:25 +0700

    commit

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export const Item = ({ image, name, order, id }) => {
 
   return (
-    <Link to= {`/product/detail/${id}`} >
+    
       <div className="product">
-      <img src={image} alt={name} />
-      <p className='name'>{name}</p>
-      <button>{order}</button>
+        <Link to= {`/product/detail/${id}`}><img src={image} alt={name} /></Link>
+        <p className='name'>{name}</p>
+        <Link to= {`/product/detail/${id}`}><button>{order}</button></Link>
     </div>
-    </Link>
+    
 
   );
 };
